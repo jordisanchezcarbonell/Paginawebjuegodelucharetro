38ea333e54c6f0afa0faa59353ecfdaf5d7b13e1
arianxa <pro.arantxa.ordoyo@gmail.com>
2020-04-26 13:21:18 +0200

    ryu

--- proyectoarantxayjordi/src/componentes/PaginaPersonajes.js
+++ proyectoarantxayjordi/src/componentes/PaginaPersonajes.js
@@ -7,7 +7,18 @@ export class PaginaPersonajes extends Component{
 
         return (
 
-            <h1>Vengo de la pagina Personajes</h1>
+            <ReactBootstrap.Card style={{ width: '18rem' }}>
+                {/*Para coger la carpeta public */}
+  <ReactBootstrap.Card.Img variant="top"  src={process.env.PUBLIC_URL +'imagenes/ryu.jpg'} />
+  <ReactBootstrap.Card.Body>
+    <ReactBootstrap.Card.Title>Card Title</ReactBootstrap.Card.Title>
+    <ReactBootstrap.Card.Text>
+      Some quick example text to build on the card title and make up the bulk of
+      the card's content.
+    </ReactBootstrap.Card.Text>
+    <ReactBootstrap.Button variant="primary">Go somewhere</ReactBootstrap.Button>
+  </ReactBootstrap.Card.Body>
+</ReactBootstrap.Card>
         )}
     
     
