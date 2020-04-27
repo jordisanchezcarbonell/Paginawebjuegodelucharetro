d71a16f80de5f2cddcd80fd67734238b7b2394f7
arianxa <pro.arantxa.ordoyo@gmail.com>
2020-04-27 10:43:11 +0200

    cards

--- proyectoarantxayjordi/src/componentes/PaginaPersonajes.js
+++ proyectoarantxayjordi/src/componentes/PaginaPersonajes.js
@@ -1,69 +1,87 @@
-import React , {Component} from 'react';
+import React, { Component } from 'react';
 import * as ReactBootstrap from 'react-bootstrap';
 import { PaginaPlataformas } from './PaginaPlataformas';
 import { PaginaJuegos } from './PaginaJuegos';
 import { DetallesPersonajes } from './DetallesPersonajes';
+import data from "../JSON/Personajes.json";
+
+
 import './Personajes.css';
-import {CargarDatos} from './CargarDatos'
+import { CargarDatos } from './CargarDatos'
 
 import {
     BrowserRouter as Router,
     Switch,
     Route,
     Link
-  } from "react-router-dom";
+} from "react-router-dom";
 
 
-export class PaginaPersonajes extends Component{
+export class PaginaPersonajes extends Component {
 
 
-    click(e){
+    click(e) {
         //const mother = new Mother();
         //mother.addMother(id);
-  
-        
+
+
         console.log();
 
-      }
-
-    render(){
-
-      console.log('-> render')
-        return (
-         
-
-            <Router>
-            <ReactBootstrap.Card onClick={this.click}  href="http://escuela.it " style={{ width: '15rem'  }}>
-            <ReactBootstrap.Nav.Link href="/DetallesPersonajes">Personajes</ReactBootstrap.Nav.Link>
-
-                {/*Para coger la carpeta public */}
-  <ReactBootstrap.Card.Img variant="top"  src={process.env.PUBLIC_URL +'imagenes/ryu.jpg'} />
-  <ReactBootstrap.Card.Body>
-    <ReactBootstrap.Card.Title>Card Title</ReactBootstrap.Card.Title>
-    <ReactBootstrap.Card.Text>
-      Some quick example text to build on the card title and make up the bulk of
-      the card's content.
-    </ReactBootstrap.Card.Text>
-    <a href="/DetallesPersonajes" className="btn btn-primary stretched-link">Go somewhere</a> 
-    
-     </ReactBootstrap.Card.Body>
-
-
-
-<Switch>
-              {/* Rutas de las paginas */}
-            
-
-              <Route path="/DetallesPersonajes">
-                <DetallesPersonajes />
-              </Route>
-            </Switch>
-            </ReactBootstrap.Card>
-        </Router>
-            
-        )
-      }
-    
-    
-    
     }
+
+    render() {
+
+        console.log('-> render')
+        return (
+            <div>
+
+
+
+                <Router>
+
+
+                {data.map((personajes, index) => {
+          return (
+            <div key={personajes.Nombre} className="MoviesList-item">
+          
+
+          
+
+ 
+             
+                    <ReactBootstrap.Card onClick={this.click} href="http://escuela.it " style={{ width: '15rem' }}>
+                        <ReactBootstrap.Nav.Link href="/DetallesPersonajes">Personajes</ReactBootstrap.Nav.Link>
+
+                        {/*Para coger la carpeta public */}
+                        <ReactBootstrap.Card.Img variant="top" src={process.env.PUBLIC_URL + personajes.Foto}  alt="Error" />
+                        <ReactBootstrap.Card.Body>
+                            <ReactBootstrap.Card.Title>{personajes.Nombre}</ReactBootstrap.Card.Title>
+                            <ReactBootstrap.Card.Text>         {personajes.Descripcion}    </ReactBootstrap.Card.Text>
+                            <a href="/DetallesPersonajes" className="btn btn-primary stretched-link">Go somewhere</a>
+
+                        </ReactBootstrap.Card.Body>
+
+
+
+                        <Switch>
+                            {/* Rutas de las paginas */}
+
+
+                            <Route path="/DetallesPersonajes">
+                                <DetallesPersonajes />
+                            </Route>
+                        </Switch>
+                    </ReactBootstrap.Card>
+                    </div>
+
+);
+})}
+                </Router>
+                
+            </div>
+        )
+    }
+
+
+
+}
