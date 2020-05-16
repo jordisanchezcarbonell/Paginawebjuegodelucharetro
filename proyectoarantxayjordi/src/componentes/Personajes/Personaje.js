39aa058ca6b510eca83519520136d60d97383856
Jordi <kazukunpayeer@gmail.com>
2020-05-16 18:34:26 +0200

    wwewqeqw

--- proyectoarantxayjordi/src/componentes/Personajes/Personaje.js
+++ proyectoarantxayjordi/src/componentes/Personajes/Personaje.js
@@ -1,19 +1,51 @@
 import React, { useState } from "react";
 import { withRouter } from "react-router-dom";
+import { Modal, Button } from "react-bootstrap";
 
 class Personaje extends React.Component {
-  constructor(props) {
-    super(props);
+  constructor(props, context) {
+    super(props, context);
+    this.handleShow = this.handleShow.bind(this);
+    this.handleClose = this.handleClose.bind(this);
+
+    this.state = {
+      show: false,
+    };
   }
 
+  handleClose() {
+    this.setState({ show: false });
+  }
+
+  handleShow() {
+    this.setState({ show: true });
+  }
   render() {
     const detalles = this.props.location.state.objecto;
     let comp;
 
-    console.log(detalles.Ataques);
+    console.log(this.state.visible);
 
     return (
       <div>
+        <Button variant="primary" onClick={this.handleShow}>
+          Launch demo modal
+        </Button>
+
+        <Modal show={this.state.show} onHide={this.handleClose}>
+          <Modal.Header closeButton>
+            <Modal.Title>Modal heading</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={this.handleClose}>
+              Close
+            </Button>
+            <Button variant="primary" onClick={this.handleClose}>
+              Save Changes
+            </Button>
+          </Modal.Footer>
+        </Modal>
         <button onClick={this.props.history.goBack}>Back</button>
 
         <h1>{comp}</h1>
@@ -29,8 +61,9 @@ class Personaje extends React.Component {
           {detalles.Ataques.map((Ataque) => (
             <li key={Ataque.nombreAtaque}>
               {Ataque.nombreAtaque}
-              {Ataque.Animacion.map((animaciondelataque) => (
+              {Ataque.Animacion.map((animaciondelataque, index) => (
                 <img
+                  key={index}
                   variant="top"
                   src={process.env.PUBLIC_URL + animaciondelataque}
                   alt="Error"
