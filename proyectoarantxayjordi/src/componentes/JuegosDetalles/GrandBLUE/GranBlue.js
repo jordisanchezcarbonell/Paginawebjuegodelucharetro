c37938aa2d0f1c99f05894bd66a1cec407dd9110
Jordi <kazukunpayeer@gmail.com>
2020-05-26 12:24:51 +0200

    Update GranBlue.js

--- proyectoarantxayjordi/src/componentes/JuegosDetalles/GrandBLUE/GranBlue.js
+++ proyectoarantxayjordi/src/componentes/JuegosDetalles/GrandBLUE/GranBlue.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Table from "@material-ui/core/Table";
 import TableBody from "@material-ui/core/TableBody";
@@ -12,13 +12,16 @@ import { Container, Row, Col } from "reactstrap";
 import "./Mortalkombat.css";
 import { withRouter } from "react-router-dom";
 import { Modal, Button } from "react-bootstrap";
+import Image from "react-bootstrap/Image";
+import { Link, animateScroll as scroll } from "react-scroll";
+
 // get our fontawesome imports
 import { faHome } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { ReactComponent as Logo } from "../nombre.png";
 import ReactPlayer from "react-player";
 import IcomoonReact, { iconList } from "icomoon-react";
-import { BrowserRouter, Route, Link } from "react-router-dom";
+import { BrowserRouter, Route } from "react-router-dom";
 var listOfImages = [];
 function importAll(r) {
   return r.keys().map(r);
@@ -29,8 +32,82 @@ function Welcome(props) {
 }
 //AVER SI FUNCIOONA
 class Child extends React.Component {
+  constructor(props) {
+    super(props);
+    console.log(props);
+    this.myRef = React.createRef(); // Create a ref object
+  }
+
   render() {
-    return <div>I'm the child</div>;
+    const styleTable = {
+      overflowX: undefined,
+      overflowY: undefined,
+    };
+    return (
+      <div
+        id="section1"
+        style={{ width: "100%" }}
+        key={this.props.elemento.Nombre}
+      >
+        <div class="horiz-rule  mt-1">
+          <div class="bar"></div>
+          <img
+            src="https://cdn-prod.mortalkombat.com/aftermath/global/vectors/horizontal-rule-node.svg"
+            alt=""
+            class="node"
+          />
+        </div>
+
+        <div class="title-block enter">
+          <h3>{this.props.elemento.Nombre}</h3>
+          <div class="rule "></div>
+        </div>
+        <Container className="">
+          <Row className="w-100  px-0 mx-0">
+            <Col className="col-md-4 w-100">
+              <TableContainer
+                component={Paper}
+                className="fondoTablaMK pruebadelelemento mt-5"
+              >
+                <h3 className="TituloAtaquesMK">Kombo Attacks</h3>
+
+                <Table
+                  stickyHeader
+                  aria-label="sticky table"
+                  className="nomostrarscroll"
+                >
+                  <TableBody className="nomostrarscroll">
+                    {this.props.elemento.Ataques.map((Ataque, index) => (
+                      <TableRow
+                        key={Ataque.nombreAtaque}
+                        className="mx-5 pruebaaa"
+                      >
+                        <TableCell
+                          className="  py-2 colorFondoAtaquesMK "
+                          scope="row"
+                          component="tr"
+                          align="left"
+                        >
+                          {Ataque.nombreAtaque}
+                        </TableCell>
+
+                        {
+                          <TableCell
+                            className="  nombreAtaquesMK py-2"
+                            component="tr"
+                            align="right"
+                          ></TableCell>
+                        }
+                      </TableRow>
+                    ))}
+                  </TableBody>
+                </Table>
+              </TableContainer>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    );
   }
 }
 
@@ -64,7 +141,42 @@ class GranBlue extends React.Component {
     imagen: "",
     isActive: false,
   };
-
+  renderContent(personaje) {
+    this.state.isItemContentVisible = false;
+    /*console.log(personaje);
+    return <div className="pruebadelelemento">prueba</div>;
+    */
+    // this.setState({ objetoPersonaje: personaje });
+    this.state.objetoPersonaje = personaje;
+    console.log(this.state.objetoPersonaje);
+  }
+  onClickDown = () => {
+    scroll.scrollToBottom();
+  };
+  scrollToTop = () => {
+    scroll.scrollToBottom();
+  };
+  showContent(id) {
+    // merge new value with existing visibility status into new object´
+    console.log(id);
+    this.setState({
+      isItemContentVisible: {
+        ...this.state.isItemContentVisible,
+        [id]: true,
+      },
+    });
+    this.setState({ mostrarsegundoelemnto: true });
+    this.onClickDown();
+  }
+  state = {
+    objetoPersonaje: "",
+    mostrarsegundoelemnto: false,
+    isItemContentVisible: false,
+    show: false,
+    elmento: "",
+    imagen: "",
+    isActive: false,
+  };
   showModal = (e, imagenes) => {
     this.setState({
       show: true,
@@ -142,7 +254,6 @@ class GranBlue extends React.Component {
             <Container className="w-100 ">
               <Row className=" w-100 stage">
                 <div class="characterMk nav nav-tabs" role="tablist">
-            
                   {this.props.Juego.map((personaje, index) => {
                     return (
                       <div
@@ -157,19 +268,17 @@ class GranBlue extends React.Component {
                       >
                         <div class="unskew.active">
                           <div class="characterMk">
-                          
-                         
                             <img
+                              href="#una-id"
+                              onClick={() => this.showContent(index)}
                               src={process.env.PUBLIC_URL + personaje.Foto}
                             ></img>
                           </div>
                         </div>
                         <div>
                           <div>
-                            
                             {this.state.isItemContentVisible[index] &&
-                              this.renderContent()}
-                          
+                              this.renderContent(personaje)}
 
                             <div></div>
                           </div>
@@ -180,6 +289,9 @@ class GranBlue extends React.Component {
                 </div>
               </Row>
             </Container>
+            {this.state.mostrarsegundoelemnto && (
+              <Child elemento={this.state.objetoPersonaje} />
+            )}
           </div>
         </div>
       </div>
