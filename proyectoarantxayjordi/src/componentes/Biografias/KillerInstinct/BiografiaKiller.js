834813738fc3c5714b675ed98b83eda65dc6b5ca
Jordi <kazukunpayeer@gmail.com>
2020-06-16 14:38:04 +0200

    killer

--- proyectoarantxayjordi/src/componentes/Biografias/KillerInstinct/BiografiaKiller.js
+++ proyectoarantxayjordi/src/componentes/Biografias/KillerInstinct/BiografiaKiller.js
@@ -4,6 +4,9 @@ import "./BiografiaKiller.css";
 
 import { withRouter } from "react-router-dom";
 import { Container, Row, Col } from "reactstrap";
+import ResponsivePlayer from "../../video/ResponsivePlayer";
+import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 class BiografiaKiller extends React.Component {
   constructor(props, context) {
@@ -17,7 +20,9 @@ class BiografiaKiller extends React.Component {
     elmento: "",
     imagen: "",
   };
-
+  componentDidMount() {
+    window.scrollTo(0, 0);
+  }
   showModal = (e, imagenes) => {
     this.setState({
       show: true,
@@ -44,8 +49,14 @@ class BiografiaKiller extends React.Component {
     };
     return (
       <div className="BioDivTotalKI">
-        
         <div style={({ width: "100%" }, fondoKIBIO)}>
+          <div>
+            <FontAwesomeIcon
+              className="iconoBD"
+              icon={faArrowLeft}
+              onClick={this.props.history.goBack}
+            />
+          </div>
           <h1 className="BioNombreKI"> {detalles.Nombre}</h1>
 
           <div>
@@ -61,11 +72,23 @@ class BiografiaKiller extends React.Component {
               <Col className="col-md-4  BIOCentrarColDivKI">
                 <h1 className="BiotituloKI"> Biografia</h1>
                 <h6 className="BioDesPonys"> {detalles.Descripcion}</h6>
+                <h1 className="BiotituloKI"> Personality</h1>
+                <h6 className="BioDesPonys"> {detalles.Personality}</h6>
+                <h1 className="BiotituloKI"> Characteristics </h1>
+
+                <h6 className="BioDesPonys">
+                  <strong>Size: </strong> {detalles.Size}
+                </h6>
+                <h6 className="BioDesPonys">
+                  <strong> Weight: </strong> {detalles.Weight}
+                </h6>
               </Col>
             </Row>
             <Row className="BioImgyBioKI">
               <h1 className="BIOtitulocombosKI"> Combos</h1>
             </Row>
+            <ResponsivePlayer className="video" url={detalles.Combos} />
+            <ResponsivePlayer className="video" url={detalles.Combos2} />
           </div>
         </div>
       </div>
