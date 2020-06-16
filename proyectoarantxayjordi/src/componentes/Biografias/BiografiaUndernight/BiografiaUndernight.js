e991eeca903ecfebbce634daef57c68b852c5c65
Jordi <kazukunpayeer@gmail.com>
2020-06-16 15:49:36 +0200

    undernight

--- proyectoarantxayjordi/src/componentes/Biografias/BiografiaUndernight/BiografiaUndernight.js
+++ proyectoarantxayjordi/src/componentes/Biografias/BiografiaUndernight/BiografiaUndernight.js
@@ -3,7 +3,9 @@ import React from "react";
 import "./BiografiaUndernight.css";
 import { Container, Row, Col } from "reactstrap";
 import { withRouter } from "react-router-dom";
-
+import ResponsivePlayer from "../../video/ResponsivePlayer";
+import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 class BiografiaUndernight extends React.Component {
   constructor(props, context) {
     super(props);
@@ -34,34 +36,52 @@ class BiografiaUndernight extends React.Component {
 
     return (
       <div className="BioDivTotalUN">
-        
-      <div className="BioEspTopUN">
-        <h1 className="BioNombreUN"> {detalles.Nombre}</h1>
+        <div className="BioEspTopUN">
+          <h1 className="BioNombreUN"> {detalles.Nombre}</h1>
 
-        <div>
-          <Row className="BioImgyBioUN">
-            <Col className="col-md-5">
-              <img
-                className="BioImgUN"
-                variant="top"
-                src={process.env.PUBLIC_URL + detalles.Foto2}
-                alt="Error"
-              />
-            </Col>
-            <Col className="col-md-5  BIOCentrarColDivUN">
-              <h1 className="BiotituloUN"> Biografia</h1>
-              <h6 className="BioDescUN"> {detalles.Descripcion}</h6>
-            </Col>
-          </Row>
-          <div className="">
-            <h1 className="BIOtitulocombosUN"> Combos</h1>
-            <div>
-              
+          <div>
+            <Row className="BioImgyBioUN">
+              <Col className="col-md-5">
+                <img
+                  className="BioImgUN"
+                  variant="top"
+                  src={process.env.PUBLIC_URL + detalles.Foto2}
+                  alt="Error"
+                />
+              </Col>
+              <Col className="col-md-5  BIOCentrarColDivUN">
+                <h1 className="BiotituloUN"> Biografia</h1>
+                <h6 className="BioDescUN"> {detalles.Descripcion}</h6>
+                <h1 className="BiotituloBD"> Personality</h1>
+                <h6 className="BioDescBD"> {detalles.Personality}</h6>
+                <h1 className="BiotituloBD"> Characteristics </h1>
+
+                <h6 className="BioDescBD">
+                  <strong>Size: </strong> {detalles.Size}
+                </h6>
+                <h6 className="BioDescBD">
+                  <strong> Weight: </strong> {detalles.Weight}
+                </h6>
+              </Col>
+            </Row>
+            <div className="">
+              <h1 className="BIOtitulocombosUN"> Combos</h1>
+              <div className="derechaBD">
+                <ResponsivePlayer className="video" url={detalles.Combos} />
+                {/* <Col className="col-md-5 ">
+                <ResponsivePlayer url={detalles.Combos} />
+                </Col>
+                <Col className="col-md-5 ">
+                <ResponsivePlayer url={detalles.Combos2} />
+              </Col> */}
+              </div>
+              <div className="izqBD">
+                <ResponsivePlayer className="video" url={detalles.Combos2} />
+              </div>
             </div>
           </div>
         </div>
-        </div>
-    </div>
+      </div>
     );
   }
 }
