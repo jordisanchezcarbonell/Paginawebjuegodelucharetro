1f9980bebd52b1e1e8ef428e03d4bfab904970bc
arianxa <pro.arantxa.ordoyo@gmail.com>
2020-06-18 16:58:21 +0200

    j

--- proyectoarantxayjordi/src/componentes/Biografias/BiografiaMK/BiografiaMK.js
+++ proyectoarantxayjordi/src/componentes/Biografias/BiografiaMK/BiografiaMK.js
@@ -1,7 +1,10 @@
 import React from "react";
 
 import "./BiografiaMK.css";
-
+import { Container, Row, Col } from "reactstrap";
+import ResponsivePlayer from "../../video/ResponsivePlayer";
+import { faArrowLeft } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { withRouter } from "react-router-dom";
 
 class BiografiaMK extends React.Component {
@@ -33,15 +36,51 @@ class BiografiaMK extends React.Component {
     const detalles = this.props.location.state.ALL;
 
     return (
+      <div className="DivBioMK">
+         <div  className="BioDivTotalMK" >
+      
+      <h1 className="BioNombreMK"> {detalles.Nombre}</h1>
+
       <div>
-        <h1 className="titulocolor"> {detalles.Nombre}</h1>
-        <img
-          className="imgAtaque"
-          variant="top"
-          src={process.env.PUBLIC_URL + detalles.Foto2}
-          alt="Error"
+        <Row className="BioImgyMK">
+        <div className="DivIconMK">
+        <FontAwesomeIcon
+          className="iconoMK"
+          icon={faArrowLeft}
+          onClick={this.props.history.goBack}
         />
-        <h1 className="titulocolor"> MK</h1>
+      </div>
+          <Col className="col-md-4">
+        
+            <img
+              className="BioImgMK"
+              variant="top"
+              src={process.env.PUBLIC_URL + detalles.Foto1}
+              alt="Error"
+            />
+          </Col>
+          <Col className="col-md-4  BIOCentrarColDivMK">
+            <h1 className="BiotituloMK"> Biografia</h1>
+            <h6 className="BioDesMK"> {detalles.Descripcion}</h6>
+            <h1 className="BiotituloMK"> Characteristics</h1>
+            <h6 className="BioDesMK"><strong>Real name: </strong> {detalles.RealName}</h6>
+            <h6 className="BioDesMK"><strong>Gender: </strong> {detalles.Gender}</h6>
+            <h6 className="BioDesMK"><strong>Species: </strong> {detalles.Species}</h6>
+
+
+            </Col>
+        </Row>
+        <Row className="BioImgyBioMK">
+          <h1 className="BIOtitulocombosMK"> Combos</h1>
+        </Row>
+        <div className="derechaMK">
+        <ResponsivePlayer className="video" url={detalles.Combos} />
+        </div>
+        <div className="izqMK">
+        <ResponsivePlayer className="video" url={detalles.Combos2} />
+        </div>
+      </div>
+    </div>
       </div>
 
 
