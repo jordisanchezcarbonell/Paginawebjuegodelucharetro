a9bfc2067e1bb7ce46607f3e550e4fa006fa3505
arianxa <pro.arantxa.ordoyo@gmail.com>
2020-06-26 14:16:32 +0200

    g

--- proyectoarantxayjordi/src/componentes/Componentepaginaprincipal/Probadeprincipal.js
+++ proyectoarantxayjordi/src/componentes/Componentepaginaprincipal/Probadeprincipal.js
@@ -3,10 +3,81 @@ import "./PaginaPrincipal.css";
 import Card from "react-bootstrap/Card";
 import CardDeck from "react-bootstrap/CardDeck";
 import { Link } from "react-router-dom";
+import { Container, Row, Col } from "reactstrap";
 
 class Probadeprincipal extends React.Component {
   render() {
     return (
+
+
+      <div className="DivPrinc">
+
+
+<div>
+
+<Container>
+
+  <Row>
+    <Col className="col-md-8 RankingGeneral">
+    <div >
+  <div className="header">
+  <h1>Ranking</h1>
+
+  </div>
+
+<p>
+Somos un equipo blafjsadfklasdjflsadf
+
+
+  
+</p>
+
+
+</div>
+    
+    
+    </Col>
+    <Col className="col-md-4 QuienesSomos">
+    
+    <h1>Quienes Somos</h1>
+    
+<p>
+Somos un equipo blafjsadfklasdjflsadf
+
+
+  
+</p>
+
+
+
+
+<div >
+  <div className="header">
+  <h1>Ranking</h1>
+
+  </div>
+
+<p>
+Somos un equipo blafjsadfklasdjflsadf
+
+
+  
+</p>
+
+
+</div>
+    </Col>
+  </Row>
+</Container>
+
+
+
+
+</div>
+
+
+{/* 
+     
       <CardDeck>
         <Card>
           <Link
@@ -25,26 +96,10 @@ class Probadeprincipal extends React.Component {
           </Link>
         </Card>
 
-        <Card>
-          <Card.Body>
-            <Card.Title className="colortextoprincilal">Card title</Card.Title>
-            <Card.Text className="colortextoprincilal">
-              This card has supporting text below as a natural lead-in to
-              additional content.{" "}
-            </Card.Text>
-          </Card.Body>
-        </Card>
-        <Card>
-          <Card.Body>
-            <Card.Title className="colortextoprincilal">Card title</Card.Title>
-            <Card.Text className="colortextoprincilal">
-              This is a wider card with supporting text below as a natural
-              lead-in to additional content. This card has even longer content
-              than the first to show that equal height action.
-            </Card.Text>
-          </Card.Body>
-        </Card>
-      </CardDeck>
+   
+       
+      </CardDeck> */}
+      </div>
     );
   }
 }
