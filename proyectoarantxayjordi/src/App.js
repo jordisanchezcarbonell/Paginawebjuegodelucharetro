e90e51f58c5a800fcba86057b429c8f2129a6f36
Jordi <kazukunpayeer@gmail.com>
2020-07-02 01:23:22 +0200

    mejoras

--- proyectoarantxayjordi/src/App.js
+++ proyectoarantxayjordi/src/App.js
@@ -7,7 +7,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
-    <div className="App">
+    <div className="App colorprimario">
       <PaginaPrincipal />
 
       <footer className="colorprimario">
